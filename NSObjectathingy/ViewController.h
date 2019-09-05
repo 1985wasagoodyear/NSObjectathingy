b90095f7dad768e8c4b83cb176737f615f9e0718
1985wasagoodyear <1985wasagoodyear>
2019-09-05 10:31:32 -0400

    Initial Commit

---
+++ NSObjectathingy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSObjectathingy
+//
+//  Created by K Y on 9/5/19.
+//  Copyright © 2019 K Y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NSObjectathingyTests/NSObjectathingyTests.m
@@ -0,0 +1,37 @@
+//
+//  NSObjectathingyTests.m
+//  NSObjectathingyTests
+//
+//  Created by K Y on 9/5/19.
+//  Copyright © 2019 K Y. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSObjectathingyTests : XCTestCase
+
+@end
+
+@implementation NSObjectathingyTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
